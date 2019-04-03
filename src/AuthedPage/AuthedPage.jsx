6b5cf237270ffb048b1b66f7aea9cdcd89170cc0
Chong Yang <iamyangchong@gmail.com>
2019-04-03 11:27:23 +1030

    feat(authed):authenticated component after login

---
+++ src/AuthedPage/AuthedPage.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class AuthedPage extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(userActions.getAll());
+    }
+
+    render() {
+        const { users } = this.props;
+        return (
+            <div class="row">
+                <div id="disp" className="col">
+                    <div className="mx-4 my-5">
+                        <h5>User List:</h5>
+                        <br/>
+                        {users.loading && <em>Loading users...</em>}
+                        {users.error && <span className="text-danger">ERROR: {users.error}</span>}
+                        {users.items &&
+                                <div>
+                                    <span>Page: {users.items.page} / {users.items.total_pages} </span>
+                                    <span>Total: {users.items.per_page} / {users.items.total} </span>
+                                    <ul class="list-group">
+                                        {users.items.data.map((user, index) =>
+                                            <li class="list-group-item" key={user.id}>
+                                                <img src={user.avatar} alt="avatar"/>
+                                                {user.first_name + ' ' + user.last_name}
+                                            </li>
+                                        )}
+                                    </ul>
+                                </div>
+                        }
+                        <br/>
+                        <div>
+                            <Link to="/" class="btn btn-primary float-right">Logout</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { users } = state;
+    return {
+        users
+    };
+}
+
+const connectedAuthedPage= connect(mapStateToProps)(AuthedPage);
+export { connectedAuthedPage as AuthedPage };
---
+++ src/AuthedPage/index.js
@@ -0,0 +1,1 @@
+export * from './AuthedPage';
